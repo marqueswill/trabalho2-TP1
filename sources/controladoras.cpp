96ba23f4e3fa7989f5a5e5553e1b63e63fb2040f
Willyan Marques <willyanmarquesmelo@gmail.com>
2023-05-25 17:59:02 -0300

    Changes to be committed: 	modified:   sources/controladoras.cpp

--- sources/controladoras.cpp
+++ sources/controladoras.cpp
@@ -4,6 +4,18 @@
 // Implementações dos métodos de classes controladoras.
 
 void CtrlIAInicializacao::executar() {
-    
+    // Mensagens a serem apresentadas na tela inicial.
+    string texto1 = "Selecione um dos servicos : ";
+    string texto2 = "1 - Acessar sistema.";
+    string texto3 = "2 - Cadastrar desenvolvedor.";
+    string texto4 = "3 - Encerrar execução do sistema.";
+
+    // Mensagens a serem apresentadas na tela de usuário logado.
+    string texto5 = "Selecione um dos servicos : ";
+    string texto6 = "1 - Selecionar serviços relacionados a desenvolvedor.";
+    string texto7 = "2 - Selecionar serviços relacionados a teste.";
+    string texto8 = "3 - Selecionar serviços relacionados a caso de teste.";
+    string texto9 = "4 - Encerrar sessão.";
+
     return;
 };
