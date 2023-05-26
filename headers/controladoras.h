70fd722c5940f0e3e9f74a49d02f20a26e8d7830
jpantonow <jpantonow@gmail.com>
2023-05-26 11:51:31 -0300

    ok

---
+++ headers/controladoras.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "interfaces.h"
+using namespace std;
+
+class cntrlDesenvolvedor: public IADesenvolvedor, ISDesenvolvedor{
+};
--- headers/interfaces.h
+++ headers/interfaces.h
@@ -2,12 +2,18 @@
 #include "entidades.h"
 //Declaração das Interfaces
 using namespace std;
-class IADesenvolvedor
-{
+class IADesenvolvedor{
     public:
     virtual void Cadastrar() throw(runtime_error) = 0;
-    virtual void Descadastrar(Desenvolvedor) throw(runtime_error) = 0;
-    virtual void Visualizar(Desenvolvedor) throw(runtime_error) = 0;
-    virtual void Editar(Desenvolvedor) throw(runtime_error) = 0;
+    virtual void Executar(Matricula) = 0;
     virtual ~IADesenvolvedor(){}
+};
+class ISDesenvolvedor
+{
+    public:
+    virtual bool Cadastrar_(Desenvolvedor) throw(runtime_error) = 0;
+    virtual bool Descadastrar(Matricula) throw(runtime_error) = 0;
+    virtual bool Visualizar(Desenvolvedor *) throw(runtime_error) = 0;
+    virtual bool Editar(Desenvolvedor) throw(runtime_error) = 0;
+    virtual ~ISDesenvolvedor(){}
 };
