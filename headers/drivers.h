9cfb4a5e15d8e772102ee2908648cfc8d17ee52e
Willyan Marques <willyanmarquesmelo@gmail.com>
2023-06-01 16:41:03 -0300

    Changes to be committed: 	modified:   headers/drivers.h 	renamed:    headers/testes_apresentacao -> headers/testes_apresentacao.h 	renamed:    headers/testes_integracao -> headers/testes_integracao.h

--- headers/drivers.h
+++ headers/drivers.h
@@ -1,4 +1,9 @@
 #ifndef DRIVERS_H_INCLUDED
 #define DRIVERS_H_INCLUDED
 
+/* Em teoria aqui deveriam ficar os testes dos módulos, mas meu plano
+é criar drivers que executem os testes. Ex: driverTestesDominios, driverTestesEntidades,
+driverTestesApresentacao, driverTestesIntegracao
+ */
+
 #endif  // DRIVERS_H_INCLUDED
--- headers/testes_apresentacao.h
+++ headers/testes_apresentacao.h
@@ -1,4 +1,6 @@
 #ifndef TESTES_APRESENTACAO_H_INCLUDED
 #define TESTES_APRESENTACAO_H_INCLUDED
 
+// Aqui vão ficar os testes de cada um dos módulos de apresentação
+
 #endif  // TESTES_APRESENTACAO_H_INCLUDED
